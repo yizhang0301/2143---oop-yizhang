4786577a6757b13d73b55fee06180190d902b4b8
yizhang0301 <43011146+yizhang0301@users.noreply.github.com>
2018-09-14 11:42:44 +0800

    Create main.cpp

---
+++ Assignments/PO1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include "myVector.h"
+
+using namespace std;
+
+int main() {
+	myVector v(10);
+	int x = v.pop_back();
+	v.push_back(13);
+	v.push_back(31);
+	v.push_back(73);
+	v.print();
+	for (int i = 0; i < 10000; i++) {
+		v.push_back(i);
+	}
+	cout << v.size() << "\n";
+	for (int i = 0; i < 998; i++) {
+		x = v.pop_back();
+	}
+	cout << v.size() << "\n";
+	v.print();
+	return 0;
+
